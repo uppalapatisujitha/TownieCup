5aaa3928b66cc9c3123e049cf750e26f097312ea
uppalapatisujitha <uppalas@bgsu.edu>
2016-03-06 10:30:57 -0600

    Added files via upload

---
+++ settingsVieww.js
@@ -0,0 +1,13 @@
+user status
+base user = G_P
+golfer = G_C
+Team captian = T_C
+Admin = A_C
+
+
+
+var AdminSettings = $("<li> <div class='dropdown pull-right'><a button class='btn btn-default pull-right' data-toggle='dropdown' href='#' onclick='return false;'>Settings <span class='caret'></span></a><ul class='dropdown-menu'><li><a href='#'>Create a team</a></li><li><a href='joinTeam.html'>Join a team</a></li><li><a href='#'>Pending Teams</a></li><li><a href='#'>Approved Teams</a></li><li><a href='#'>Pending Teams</a></li><li><a href='#'>Pending Team Members</a></li><li><a href='#'>Approved Team Members</a></li><li><a href='#'>Change Password</a></li></ul></div></li>");
+var TCSettings = $("<li> <div class='dropdown pull-right'><a button class='btn btn-default pull-right' data-toggle='dropdown' href='#' onclick='return false;'>Settings <span class='caret'></span></a><ul class='dropdown-menu'><li><a href='#'>Pending Team Members</a></li><li><a href='#'>Approved Team Members</a></li><li><a href='#'>Change Password</a></li></ul></div></li>");
+var GCSettings = $("<li> <div class='dropdown pull-right'><a button class='btn btn-default pull-right' data-toggle='dropdown' href='#' onclick='return false;'>Settings <span class='caret'></span></a><ul class='dropdown-menu'><li><a href='#'>Approved Team Members</a></li><li><a href='#'>Change Password</a></li></ul></div></li>");
+var GCSettings = $("<li> <div class='dropdown pull-right'><a button class='btn btn-default pull-right' data-toggle='dropdown' href='#' onclick='return false;'>Settings <span class='caret'></span></a><ul class='dropdown-menu'><li><a href='#'>Approved Team Members</a></li><li><a href='#'>Change Password</a></li></ul></div></li>");
+var GPSettings = $("<li> <div class='dropdown pull-right'><a button class='btn btn-default pull-right' data-toggle='dropdown' href='#' onclick='return false;'>Settings <span class='caret'></span></a><ul class='dropdown-menu'><li><a href='#'>Create a team</a></li><li><a href='joinTeam.html'>Join a team</a></li><li><a href='#'>Change Password</a></li></ul></div></li>");
